964a31d932941833d207124de286e2aab09332ca
Bruce Zheng <53232883+zmx-bruce@users.noreply.github.com>
2021-07-19 22:30:50 +0800

    Add files via upload

--- model/save_preds.py
+++ model/save_preds.py
@@ -3,26 +3,23 @@
 
 # ## Saving Predictions
 
-# In[ ]:
+# In[2]:
 
 
 from pathlib import Path
-
-#data_dir = Path("/home/jovyan/data")
-#process_dir = data_dir / "processed"
 args = {
-    "device": "cpu", # set to "cuda" if gpu is available
-    "out_dir": "predictions"
+    "device": "cuda", # set to "cuda" if gpu is available
+    "out_dir":"predictions"
 }
 
 
-# In[ ]:
+# In[4]:
 
 
 import torch
 from unet import Unet
 
-state = torch.load("model.pt",map_location=args["device"])
+state = torch.load(Path("model.pt"), map_location=args["device"])
 model = Unet(10, 3, 4).to(args["device"])
 model.load_state_dict(state)
 model = model.eval()
@@ -33,16 +30,12 @@ model = model.eval()
 
 from data import GlacierDataset
 from torch.utils.data import DataLoader
-
 paths = {}
 for split in ["train", "test"]:
     paths[split] = {}
     for v in ["x", "y"]:
         paths[split][v] = list(Path(split+"_npy/test/processed").glob(v + "*"))
         paths[split][v].sort()
-print(paths["train"])
-print(" ")
-print(paths["test"])
 ds = {
     "train": GlacierDataset(paths["train"]["x"], paths["train"]["y"]),
     "test": GlacierDataset(paths["test"]["x"], paths["test"]["y"])
@@ -52,8 +45,14 @@ ds = {
 # In[ ]:
 
 
-from train_test import predictions
+from train import predictions
 
-predictions(model, ds["train"], Path(args["out_dir"]+"/"+"train"),args["device"])
-predictions(model, ds["test"], Path(args["out_dir"]+"/"+"test"), args["device"])
+predictions(model, ds["train"], args["out_dir"] / "train", args["device"])
+predictions(model, ds["test"], args["out_dir"] / "test", args["device"])
+
+
+# In[ ]:
+
+
+
 
