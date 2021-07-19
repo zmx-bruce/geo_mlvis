9ff163e56b66ed89bd761011e1b203fc0981d70b
Bruce Zheng <53232883+zmx-bruce@users.noreply.github.com>
2021-07-19 09:51:14 +0800

    Add files via upload

--- model/train_gpu.py
+++ model/train_gpu.py
@@ -7,7 +7,7 @@
 import urllib.request
 import tarfile
 from pathlib import Path
-#from data import create_dir, download_data
+from data import create_dir, download_data
 import os
 
 
@@ -22,28 +22,21 @@ args = {
 }
 
 
-# In[21]:
-
-
-import numpy as np
-np.load(paths['x'][0]).shape
-
-
 # In[22]:
+
 
 
 from data import GlacierDataset
 from torch.utils.data import DataLoader
-path="/Users/app/Desktop/geo_mlvis/test/processed"
-#path="/Volumes/KINGSTON/Remote Sensing Presentation/preprocessed/train"
+
+path=Path('npy/test/processed')
 paths = {
-    "x": list(Path(path).glob("x*")),
-    "y": list(Path(path).glob("y*"))
+    "x": list(path.glob("x*")),
+    "y": list(path.glob("y*"))
 }
 
 ds = GlacierDataset(paths["x"], paths["y"])
 loader = DataLoader(ds, batch_size=args["batch_size"], shuffle=True)
-
 
 # In[27]:
 
@@ -56,12 +49,11 @@ model = Unet(10, 3, 4, dropout=0.2).to(args["device"])
 optimizer = torch.optim.Adam(model.parameters(), lr=args["lr"])
 Loss_Total=[];Loss_Batch=[]
 for epoch in range(args["epochs"]):
-    train_epoch(model, loader, optimizer, args["device"], epoch)
-    #Loss_Total.append(l[0])
-    #Loss_Batch.append(l[1])
+    l=train_epoch(model, loader, optimizer, args["device"], epoch)
+    Loss_Total.append(l[0])
+    Loss_Batch.append(l[1])
     
-torch.save(model.state_dict(), "model.pt")
-
+torch.save(model.state_dict(),"model.pt")
 
 # In[ ]:
 
