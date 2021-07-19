829c7e0762171beb6414713cd159e75d9115b903
Bruce Zheng <53232883+zmx-bruce@users.noreply.github.com>
2021-07-19 09:58:04 +0800

    Update train_gpu.py

--- model/train_gpu.py
+++ model/train_gpu.py
@@ -31,10 +31,10 @@ from torch.utils.data import DataLoader
 
 path=Path('npy/test/processed')
 paths = {
-    "x": list(path.glob("x*")),
-    "y": list(path.glob("y*"))
+    "x": list(path.glob("x-0-1*")),
+    "y": list(path.glob("y-0-1*"))
 }
-
+print('paths success')
 ds = GlacierDataset(paths["x"], paths["y"])
 loader = DataLoader(ds, batch_size=args["batch_size"], shuffle=True)
 
@@ -47,8 +47,10 @@ from train import train_epoch
 
 model = Unet(10, 3, 4, dropout=0.2).to(args["device"])
 optimizer = torch.optim.Adam(model.parameters(), lr=args["lr"])
+print('model and optimize success')
 Loss_Total=[];Loss_Batch=[]
 for epoch in range(args["epochs"]):
+    print('epoch success')
     l=train_epoch(model, loader, optimizer, args["device"], epoch)
     Loss_Total.append(l[0])
     Loss_Batch.append(l[1])
