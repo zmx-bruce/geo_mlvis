a7848207af4a915f3d3a906ef508c9f0d7404a15
Bruce Zheng <53232883+zmx-bruce@users.noreply.github.com>
2021-08-09 15:46:39 +0800

    Update save_preds_test.py

--- model/save_preds_test.py
+++ model/save_preds_test.py
@@ -1,27 +1,20 @@
 from pathlib import Path
 
-data_dir = Path("/home/jovyan/data")
-process_dir = data_dir / "processed"
+#data_dir = Path("/home/jovyan/data")
+#process_dir = data_dir / "processed"
 args = {
-    "device": "cpu", # set to "cuda" if gpu is available
-    "out_dir": data_dir / "predictions"
+    "device": "cuda", # set to "cuda" if gpu is available
+    "out_dir": "predictions"
 }
 
-from data import download_data
+#from data import download_data
 
-links = {
-    "test_data": "https://uwmadison.box.com/shared/static/zs8vtmwbl92j5oq6ekzcfod11ym1w599.gz",
-    "model": "https://uwmadison.box.com/shared/static/byb5lpny6rjr15zbx28o8liku8g6nga6.pt"
-}
-
-download_data(links["test_data"], process_dir / "test.tar.gz")
-download_data(links["model"], data_dir / "model.pt", unzip = False)
 
 import torch
 from unet import Unet
 
-state = torch.load(data_dir / "model.pt", map_location=args["device"])
-model = Unet(13, 3, 4).to(args["device"])
+state = torch.load("model.pt", map_location=args["device"])
+model = Unet(10, 3, 4).to(args["device"])
 model.load_state_dict(state)
 model = model.eval()
 
@@ -29,18 +22,32 @@ from data import GlacierDataset
 from torch.utils.data import DataLoader
 
 paths = {}
-for split in ["train", "test"]:
+#for split in ["train", "test"]:
+for split in ["train"]:
     paths[split] = {}
     for v in ["x", "y"]:
-        paths[split][v] = list((process_dir / split).glob(v + "*"))
+       #paths[split][v] = list(Path(split+path).glob(v + "*"))
+        paths[split][v] = list(Path("npy").glob(v + "*"))
         paths[split][v].sort()
-
 ds = {
     "train": GlacierDataset(paths["train"]["x"], paths["train"]["y"]),
-    "test": GlacierDataset(paths["test"]["x"], paths["test"]["y"])
+    #"test": GlacierDataset(paths["test"]["x"], paths["test"]["y"])
 }
+
+
+#paths = {}
+#for split in ["train", "test"]:
+    #paths[split] = {}
+    #for v in ["x", "y"]:
+        #paths[split][v] = list((process_dir / split).glob(v + "*"))
+        #paths[split][v].sort()
+
+#ds = {
+    #"train": GlacierDataset(paths["train"]["x"], paths["train"]["y"]),
+    #"test": GlacierDataset(paths["test"]["x"], paths["test"]["y"])
+#}
 
 from train import predictions
 
 predictions(model, ds["train"], args["out_dir"] / "train", args["device"])
-predictions(model, ds["test"], args["out_dir"] / "test", args["device"])
+#predictions(model, ds["test"], args["out_dir"] / "test", args["device"])
