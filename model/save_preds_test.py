44a38aa3d6750842a46f72173a5c0aad88058b45
Bruce Zheng <53232883+zmx-bruce@users.noreply.github.com>
2021-08-09 16:11:15 +0800

    Update save_preds_test.py

--- model/save_preds_test.py
+++ model/save_preds_test.py
@@ -3,7 +3,7 @@ from pathlib import Path
 #data_dir = Path("/home/jovyan/data")
 #process_dir = data_dir / "processed"
 args = {
-    "device": "cuda", # set to "cuda" if gpu is available
+    "device": "cpu", # set to "cuda" if gpu is available
     "out_dir": "predictions"
 }
 
